b190aeced2bb01af7427839ef966227dc24b988d
amalsukhadewe <78414976+amalsukhadewe@users.noreply.github.com>
2021-02-10 00:21:42 +0530

    QUESTION AND ANSWER

---
+++ QUESTION.C
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d,e;
+	clrscr();
+	printf("\n Q1}the addition of 1+1 ?");
+	printf("\n a)0");
+	printf("\n b)1");
+	printf("\n c)2");
+	printf("\n d)3");
+	printf("\n enter your answer=");
+	scanf("\n %d",&a);
+	printf("\n Q1}the addition of 2+2 ?");
+	printf("\n a)0");
+	printf("\n b)1");
+	printf("\n c)2");
+	printf("\n d)4");
+	printf("\n enter your answer=");
+	scanf("\n %d",&b);
+	printf("\n Q1}the addition of 3+3 ?");
+	printf("\n a)0");
+	printf("\n b)1");
+	printf("\n c)2");
+	printf("\n d)6");
+	printf("\n enter your answer=");
+	scanf("\n %d",&c);
+	printf("\n Q1}the addition of 4+4 ?");
+	printf("\n a)0");
+	printf("\n b)1");
+	printf("\n c)8");
+	printf("\n d)3");
+	printf("\n enter your answer=");
+	scanf("\n %d",&d);
+	printf("\n Q1}the addition of 5+5 ?");
+	printf("\n a)0");
+	printf("\n b)10");
+	printf("\n c)2");
+	printf("\n d)3");
+	printf("\n enter your answer=");
+	scanf("\n %d",&e);
+	printf("\n correct answers of the above question are=");
+	printf("\n the correct answer of first question is==>>option c)2");
+	printf("\n you have given the answer is==%d",a);
+	printf("\n\n the correct answer of second question is==>>option d)4");
+	printf("\n you have given the answer is==%d",b);
+	printf("\n\n the correct answer of third question is==>>option d)6");
+	printf("\n you have given the answer is==%d",c);
+	printf("\n\n the correct answer of fourth question is==>>option c)8");
+	printf("\n you have given the answer is==%d",d);
+	printf("\n\n the correct answer of fifth question is==>>option b)10");
+	printf("\n you have given the answer is==%d",e);
+
+
+
+getch();
+}
